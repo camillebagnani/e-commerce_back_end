893a1fdcb367f327eba64b0e2476093316941e18
camillebagnani <140850865+camillebagnani@users.noreply.github.com>
2024-01-27 19:08:48 -0700

    product routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -18,9 +18,17 @@ router.get('/', async (req, res) => {
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product.findByPk(req.params.id, {
+      include: [Category, Tag],
+    });
+    res.json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // create new product
@@ -31,6 +39,13 @@ router.post('/', (req, res) => {
       price: 200.00,
       stock: 3,
       tagIds: [1, 2, 3, 4]
+    }
+       {
+      "product_name": "Cassette",
+      "price": 20.00,
+      "stock": 3,
+      "tagIds": [1, 2, 3, 4],
+		 "category_id": 3
     }
   */
   Product.create(req.body)
@@ -100,8 +115,23 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const productData = await Product.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if(!productData) {
+      res.status(404).json({ message: 'No product found with this id!'});
+      return;
+    }
+    res.json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
